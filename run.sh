e47c2d0ca752fea38cb1b9a92b87af50722d850b
Martin <machacekmartin@icloud.com>
2025-09-02 17:23:06 +0200

    Apply batocera logic + comments

--- run.sh
+++ run.sh
@@ -3,68 +3,73 @@
 DISK="/dev/disk4"
 
 diskutil unmountDisk $DISK
-echo "--- Unmounted disk ---"
 
-sudo diskutil partitionDisk $DISK 1 GPT "Free Space" Free R
-echo "--- Partitioned disk ---"
-
-diskutil addPartition $DISK "MS-DOS FAT32" BATOCERA_B 4GB
-echo "--- Added BATOCERA_B partition ---"
-
-diskutil addPartition $DISK ExFAT BATOCERA_R 32GB
-echo "--- Added BATOCERA_R partition ---"
-
-diskutil addPartition $DISK "MS-DOS FAT32" LIBREELEC_B 4GB
-echo "--- Added LIBREELEC_B partition ---"
-
-diskutil addPartition $DISK ExFAT LIBREELEC_R 32GB
-echo "--- Added LIBREELEC_R partition ---"
+################ PURGE AND CREATE PARTITIONS ################
+sudo diskutil partitionDisk $DISK GPT \
+    "MS-DOS FAT32" BATOCERA_B 4GB \
+    "ExFAT" BATOCERA_R 32GB \
+    "MS-DOS FAT32" LIBREELEC_B 4GB \
+    "ExFAT" LIBREELEC_R 32GB \
+    "Free Space" Free R
 
 diskutil unmountDisk $DISK
 
+
+################ FORMAT EXT4 PARTITIONS ################
 sudo $(brew --prefix e2fsprogs)/sbin/mkfs.ext4 -F ${DISK}s3
 sudo $(brew --prefix e2fsprogs)/sbin/mkfs.ext4 -F ${DISK}s5
-echo "--- Formatted ext4 partitions ---"
 
+
+################ RETRIEVE UUID OF LIBREELEC PARTITIONS (batocera does not require this) ################
 LIBREELEC_BOOT_PARTITION_IDENTIFIER=$(sudo dd if="$DISK"s4 bs=1m count=10 | file - | awk -F'serial number 0x' '{split($2,a,","); print a[1]}')
 LIBREELEC_BOOT_PARTITION_UUID="${LIBREELEC_BOOT_PARTITION_IDENTIFIER:0:4}-${LIBREELEC_BOOT_PARTITION_IDENTIFIER:4:4}"
 LIBREELEC_ROOT_PARTITION_UUID=$(sudo dd if="$DISK"s5 bs=1m count=10 | file - | grep -Eo 'UUID=[0-9a-fA-F-]+' | cut -d= -f2)
-echo "--- Retrieved UUIDs of partitions ---"
 
-diskutil unmountDisk $DISK
+diskutil mountDisk $DISK
 
-#curl batocera
-curl -L -o ./images/libreelec.img.gz https://releases.libreelec.tv/LibreELEC-RPi5.aarch64-12.2.0.img.gz
+################ RETRIEVE IMAGES ################
+if [ -f ./images/downloaded/batocera.img ]; then
+else
+    curl -L -o ./images/downloaded/batocera.img.gz https://updates.batocera.org/bcm2712/stable/last/batocera-bcm2712-41-20241217.img.gz
+    gunzip ./images/downloaded/batocera.img.gz
+fi
 
-#gunzip batocera
-gunzip ./images/libreelec.img.gz
+if [ -f ./images/downloaded/libreelec.img ]; then
+else
+    curl -L -o ./images/downloaded/libreelec.img.gz https://releases.libreelec.tv/LibreELEC-RPi5.aarch64-12.2.0.img.gz
+    gunzip ./images/downloaded/libreelec.img.gz
+fi
 
-#hdiutil attach batocera
-hdiutil attach ./images/libreelec.img -mountpoint ./images/mounted/libreelec
 
+################ ATTACH IMAGES FOR COPYING ################
+hdiutil attach ./images/downloaded/batocera.img -mountpoint ./images/mounted/batocera
+hdiutil attach ./images/downloaded/libreelec.img -mountpoint ./images/mounted/libreelec
+
+
+################ COPY CONTENTS OF IMAGES AND AUTOBOOT CONFIG ################
+cp -r ./images/mounted/batocera/* /Volumes/BATOCERA_B
+cp -r ./images/mounted/libreelec/* /Volumes/LIBREELEC_B
 cp -r ./efi/* /Volumes/EFI
-echo "--- Copied data into EFI partition ---"
 
-# cp -r ./batocera/* /Volumes/BATOCERA_B
-# echo "--- Copied data into BATOCERA_B partition ---"
 
-cp -r ./images/mounted/* /Volumes/LIBREELEC_B
-echo "--- Copied data into LIBREELEC_B partition ---"
-
-# mkdir -p /Volumes/BATOCERA_B/reboot
-mkdir -p /Volumes/LIBREELEC_B/reboot
-
-# cp -r ./reboot/* /Volumes/BATOCERA_B/reboot
-# echo "--- Copied custom reboot scripts into BATOCERA_B partition ---"
-
-cp -r ./reboot/* /Volumes/LIBREELEC_B/reboot
-echo "--- Copied custom reboot scripts into LIBREELEC_B partition ---"
-
+################ APPLY CUSTOM PARTITIONS INTO CMDLINE configuration ################
+cat <<EOF > /Volumes/BATOCERA_B/cmdline.txt
+console=tty3 loglevel=3 elevator=deadline vt.global_cursor_default=0 logo.nologo dev=LABEL=BATOCERA_B rootwait fastboot noswap
+EOF
 cat <<EOF > /Volumes/LIBREELEC_B/cmdline.txt
 boot=UUID=$LIBREELEC_BOOT_PARTITION_UUID disk=UUID=$LIBREELEC_ROOT_PARTITION_UUID quiet console=ttyAMA10,115200 console=tty0
 EOF
 
+################ COPY REBOOT SCRIPTS ################
+mkdir -p /Volumes/BATOCERA_B/reboot
+mkdir -p /Volumes/LIBREELEC_B/reboot
+cp -r ./reboot/* /Volumes/BATOCERA_B/reboot
+cp -r ./reboot/* /Volumes/LIBREELEC_B/reboot
+
+################ DETACH AND UNMOUNT ################
+hdiutil detach ./images/mounted/batocera
+hdiutil detach ./images/mounted/libreelec
 diskutil unmountDisk $DISK
 
-echo "--- DONE ---"
+echo "================================== YOUR PI IS READY =================================="
 
